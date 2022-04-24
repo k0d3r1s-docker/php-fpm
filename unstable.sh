3c73a31694b1ca2fcb61f0af5d55f5ba16456600
k0d3r1s <k0d3r1s@gmail.com>
2022-04-24 03:24:43 +0300

    add ev and event exts

--- unstable.sh
+++ unstable.sh
@@ -11,7 +11,7 @@ rm -rf src
 cp -r source src
 rm -rf src/.git src/.github
 
-docker build --tag k0d3r1s/php-fpm:${VERSION} --tag k0d3r1s/php-fpm:unstable --tag k0d3r1s/php-fpm:8.2.0-dev --squash --compress --no-cache -f Dockerfile.unstable  --build-arg version=${VERSION} . || exit
+docker build --tag k0d3r1s/php-fpm:${VERSION} --tag k0d3r1s/php-fpm:unstable --tag k0d3r1s/php-fpm:8.2.0-dev --squash --compress --no-cache -f Dockerfile.unstable . || exit
 
 rm -rf src
 
